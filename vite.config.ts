91e17d4bfdde898d3108b0064d276433d17037a8
Ian Burke <ian.burke3@gmail.com>
2026-03-02 22:24:31 +1100

    fix: set vite root to client for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,16 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  root: path.resolve(__dirname, "client"),
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./client/src"),
-      shared: path.resolve(__dirname, "./shared"),
+      "@": path.resolve(__dirname, "client/src"),
+      shared: path.resolve(__dirname, "shared"),
     },
+  },
+  build: {
+    outDir: path.resolve(__dirname, "dist"),
+    emptyOutDir: true,
   },
 });
