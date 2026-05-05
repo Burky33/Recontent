1e265cdaeb89efeb1b1188368c423f7b5d14c88e
Ian Burke <ian.burke3@gmail.com>
2026-05-05 23:56:04 +1000

    move GA4 to env var, trim google fonts

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,4 +3,18 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+const GA_ID = import.meta.env.VITE_GA4_MEASUREMENT_ID
+
+if (GA_ID) {
+  const script = document.createElement('script')
+  script.async = true
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`
+  document.head.appendChild(script)
+
+  window.dataLayer = window.dataLayer || []
+  window.gtag = function(...args: any[]) { window.dataLayer.push(args) }
+  window.gtag('js', new Date())
+  window.gtag('config', GA_ID)
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
