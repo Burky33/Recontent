1fb4703d165fe38c6d257b9f73b02e66cb4c836a
Ian Burke <ian.burke3@gmail.com>
2026-05-06 00:45:26 +1000

    add PostHog analytics and session replay

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import "./sentry";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import posthog from "posthog-js";
 
 const GA_ID = import.meta.env.VITE_GA4_MEASUREMENT_ID
 
@@ -15,6 +16,18 @@ if (GA_ID) {
   window.gtag = function(...args: any[]) { window.dataLayer.push(args) }
   window.gtag('js', new Date())
   window.gtag('config', GA_ID)
+}
+
+const PH_KEY = import.meta.env.VITE_POSTHOG_KEY
+const PH_HOST = import.meta.env.VITE_POSTHOG_HOST
+
+if (PH_KEY) {
+  posthog.init(PH_KEY, {
+    api_host: PH_HOST || "https://us.i.posthog.com",
+    capture_pageview: true,
+    capture_pageleave: true,
+    session_recording: { enabled: true },
+  })
 }
 
 createRoot(document.getElementById("root")!).render(<App />);
